8dc6749ca3e914c38cd8c593ff91eb100c9c0fda
Yana Ananenko <yayuananenko@edu.hse.ru>
2024-01-12 18:17:31 +0300

    deploy on linux

---
+++ Network/main.c
@@ -0,0 +1,6 @@
+#include "../eigen/Eigen/Eigen"
+#include "../EigenRand/EigenRand/EigenRand"
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
---
+++ Network/net.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yanaa on 12.01.2024.
+//
+
+#include "net.h"
